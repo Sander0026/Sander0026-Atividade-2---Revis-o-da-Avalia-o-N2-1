e9f47516d905a39c0688895fb0a79429d40766a1
alexsander ribeiro <102040731+Sander0026@users.noreply.github.com>
2025-10-29 14:00:01 -0300

    feat: Adiciona função para gerar pedidos fictícios
    A função é responsável por:
    - Utilizar a biblioteca `Faker` para criar dados realistas e aleatórios para um pedido.
    - Gerar um `PedidoID` único usando `uuid`.
    - Incluir campos essenciais como `Status` (Pendente), `Cliente`, `Data`, `Quantidade`, `PrecoUnitario`, `Latitude` e `Longitud`.
    - O objetivo é fornecer dados de teste para simular a entrada de novos pedidos no sistema.

---
+++ src/GeraPedidos.py
@@ -0,0 +1,17 @@
+from faker import Faker
+import uuid
+
+faker = Faker('pt_BR')
+
+def GeraPedidos():
+    return{
+            "PedidoID": str(uuid.uuid4()),
+            "Status": "Pendente",
+            "Cliente": faker.name(),
+            "Data": faker.date(),
+            "Quantidade": faker.random_int(min=1, max=50),
+            "PrecoUnitario": faker.random_int(min=5, max=999),
+            "Latitude": float(faker.latitude()),
+            "Longitud": float(faker.longitude()),
+        }
+     
