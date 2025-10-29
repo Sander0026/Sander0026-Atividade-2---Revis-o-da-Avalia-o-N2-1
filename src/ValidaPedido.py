5d8c11ce8b0c02b802cc4d3d43e07423ca44ce9f
alexsander ribeiro <102040731+Sander0026@users.noreply.github.com>
2025-10-29 13:56:20 -0300

    feat: Implementa validação inicial de pedidos e adição à fila
    A função é responsável por:
    - Receber um objeto de pedido (evento).
    - Validar a presença dos campos essenciais: `PedidoID`, `Latitude` e `Longitud`.
    - Se o pedido for considerado válido, ele é adicionado à `FilaPedidos` para processamento assíncrono.
    - Retorna um status e uma mensagem indicando o sucesso da validação e adição à fila, ou um erro caso campos obrigatórios estejam faltando.

--- src/ValidaPedido.py
+++ src/ValidaPedido.py
@@ -1,23 +1,24 @@
-from FilaPedido import FilaPedido
+from .FilaPedidos import FilaPedidos
 
-def ValidaPedido(pedido):
-    '''
-    {
-            "Longitud": 4,
-            "Latitude": 5,
-            "PedidoID": 2,
-            "Cliente": "Maria Oliveira",
-            "Data": "2024-06-16",
-            "Itens": [
-                {
-                    "ProdutoID": 303,
-                    "NomeProduto": "Tênis Esportivo",
-                    "Quantidade": 1,
-                    "PrecoUnitario": 199.90
-                }
-            ],
-            "TotalPedido": 199.90
-    }
-    '''
-    FilaPedido.put(pedido)
+def ValidaPedido(evento):
    
+    print(f"Validando pedido: {evento}")
+    pedido = evento
+        
+    pedido_id = evento.get('PedidoID', 'DESCONHECIDO')
+    print(f"Validando pedido: {pedido_id}")
+    
+    pedido = evento
+    try:  
+        if ('PedidoID' in pedido and
+            'Latitude' in pedido and
+            'Longitud' in pedido):
+
+            print(f"Pedido {pedido['PedidoID']} é VÁLIDO. Enviando para FilaPedido.")
+            FilaPedidos.put(pedido)
+        return {"status": 200, 
+                "mensagem": "Pedido valido e adicionado à fila."}
+    except KeyError:
+        return {"status": 500, 
+                "mensagem": "Preencha todos os campos!"}
+    
