6eb123ef0f77c9d1f0d9a500fff60e0dd531d83e
alexsander ribeiro <102040731+Sander0026@users.noreply.github.com>
2025-10-29 13:58:38 -0300

    feat: Implementa processamento de pedidos e persistência em JSON
    A função é responsável por:
    - Retirar um pedido da fila de processamento.
    - Validar a presença dos campos essenciais (`PedidoID`, `Latitude`, `Longitud`).
    - Atualizar o status do pedido para 'PROCESSADO'.
    - Persistir o pedido processado em um arquivo JSON (`BancoPedidos.json`), adicionando-o à lista de `PedidosProcessados`.
    - Retorna um status e uma mensagem indicando o sucesso do processamento e salvamento, ou um erro caso haja falha na validação ou no acesso aos dados.

--- src/ProcessaPedido.py
+++ src/ProcessaPedido.py
@@ -1,28 +1,35 @@
 import json
-from FilaPedido import FilaPedido
 
-def ProcessaPedido(fila_pedidos):
-    '''
-    Processa os pedidos na fila de pedidos.
-    '''
-    pedido = fila_pedidos.get()
-    arq = open('./schema/BancoPedidos.json', 'r')
-    dadosBanco = json.loads(arq.read())
-    arq.close()
+def ProcessaPedido(Fila):
+    print("Iniciando o processamento de pedidos...")
+    
+    pedido = Fila.get()
      
-    for pedidoBanco in dadosBanco['Pedidos']:
+    try:
+        if ('PedidoID' in pedido and 
+            'Latitude' in pedido and 
+            'Longitud' in pedido):
+            pedido['Status'] = 'PROCESSADO'
+            print(f"Pedido {pedido['PedidoID']} processado com sucesso.")
 
-        try:
-            if (pedidoBanco['PedidoID'] == pedido['PedidoID'] and 
-                pedidoBanco['Cliente'] == pedido['Cliente'] and 
-                pedidoBanco['Data'] == pedido['Data']):
-
-                return {"status": 200, 
-                        "mensagem": "Pedido processado com sucesso."}
-        
-        except KeyError:
-            return {"status": 500, 
-                    "mensagem": "Preencha todos os campos!"}
-
+            # Caminho para o arquivo JSON
+            json_path = 'e:\\Sistemas_Web\\Sander0026-Atividade-2---Revis-o-da-Avalia-o-N2-1\\docs\\BancoPedidos.json'
             
+            # Abre o arquivo para leitura e carrega os dados
+            with open(json_path, 'r', encoding='utf-8') as arq:
+                BancoPedidos = json.load(arq)
            
+            # Adiciona o novo pedido processado à lista
+            BancoPedidos["PedidosProcessados"].append(pedido)
+
+            # Abre o arquivo para escrita e salva os dados atualizados
+            with open(json_path, 'w', encoding='utf-8') as arq:
+                json.dump(BancoPedidos, arq, indent=4, ensure_ascii=False)
+
+            return {"status": 200, 
+                    "mensagem": "Pedido processado com sucesso e salvo no JSON."}
+        
+    except KeyError:
+        return {"status": 500, 
+                "mensagem": "Preencha todos os campos!"}
+
